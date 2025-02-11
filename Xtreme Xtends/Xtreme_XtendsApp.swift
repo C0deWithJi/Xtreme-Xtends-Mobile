8b455b71f607093494366c2fa2ace3e05eafabad
CodeWithJi <Codewithji@icloud.com>
2025-02-11 12:57:46 -0800

    Initial Commit

---
+++ Xtreme Xtends/Xtreme_XtendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xtreme_XtendsApp.swift
+//  Xtreme Xtends
+//
+//  Created by Jihad Vercher on 2/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Xtreme_XtendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xtreme XtendsTests/Xtreme_XtendsTests.swift
@@ -0,0 +1,17 @@
+//
+//  Xtreme_XtendsTests.swift
+//  Xtreme XtendsTests
+//
+//  Created by Jihad Vercher on 2/11/25.
+//
+
+import Testing
+@testable import Xtreme_Xtends
+
+struct Xtreme_XtendsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
