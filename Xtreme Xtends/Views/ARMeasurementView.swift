b2d134d21dd59f02acd6159c2abf2b242bab9073
C0deWithJi <Codewithji@icloud.com>
2025-02-11 19:00:45 -0800

    added ARMeasurement UI code

--- Xtreme Xtends/Views/ARMeasurementView.swift
+++ Xtreme Xtends/Views/ARMeasurementView.swift
@@ -7,11 +7,25 @@
 // AR crown measurement view
 
 import SwiftUI
+import ARKit
 
 struct ARMeasurementView: View {
+    @StateObject private var arService = ARMeasurementService()
+    
     var body: some View {
-        Text("AR Measurement")
+        ZStack {
+            ARViewContainer(arView: arService.arView)
+            Text("Crown size: \(String(format: "%.if", arService.crownMeasurement)) cm")
+                .font(.title)
+        }
     }
+}
+
+struct ARViewContainer: UIViewRepresentable {
+    let arView: ARSCNView
+    
+    func makeUIView(context: Context) -> ARSCNView { arView }
+    func updateUIView(_ uiView: ARSCNView, context: Context) {}
 }
 
 #Preview {
