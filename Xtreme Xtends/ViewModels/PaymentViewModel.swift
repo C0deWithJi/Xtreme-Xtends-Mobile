cc268b34ec22bfd192f06c7329c7d0d73dd77218
C0deWithJi <Codewithji@icloud.com>
2025-02-12 16:44:59 -0800

    added payment view model code

--- Xtreme Xtends/ViewModels/PaymentViewModel.swift
+++ Xtreme Xtends/ViewModels/PaymentViewModel.swift
@@ -7,3 +7,33 @@
 // Handles shopify integration
 
 import Foundation
+import Buy
+
+struct CartItem {
+    let variantId: GraphQL.ID
+    let quantity: Int32
+}
+
+struct PaymentData {
+    let cardNumber: String
+    let expiryMonth: String
+    let expiryYear: String
+    let cvv: String
+    let firstName: String
+    let lastName: String
+    let billingAddress: Address
+    let amount: Decimal
+}
+
+// In Models/Address.swift
+// In Models/Address.swift
+struct Address {
+    let firstName: String
+    let lastName: String
+    let address1: String
+    let address2: String?
+    let city: String
+    let province: String?
+    let zip: String
+    let countryCode: String // e.g., "US", "CA"
+}
