b6c2549cdbeec2a0def0ac9ed7b87a7d6f4df29f
C0deWithJi <Codewithji@icloud.com>
2025-02-12 16:45:55 -0800

    added marketplace view code

--- Xtreme Xtends/Views/MarketplaceView.swift
+++ Xtreme Xtends/Views/MarketplaceView.swift
@@ -4,16 +4,49 @@
 //
 //  Created by Jihad Vercher on 2/11/25.
 //
-// Product Listings
+// Displays a grid/list of wigs fetched from shopify
 
 import SwiftUI
 
 struct MarketplaceView: View {
+    @StateObject private var viewModel = MarketplaceViewModel()
+    
     var body: some View {
-        Text("Marketplace")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())]) {
+                    ForEach(viewModel.products) { product in
+                        ProductCard(product: product)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle("Xtreme Xtends")
+            .onAppear {
+                viewModel.fetchProducts()
+            }
+        }
     }
 }
 
-#Preview {
-    MarketplaceView()
+// Simple product card
+struct ProductCard: View {
+    let product: Product
+    
+    var body: some View {
+        VStack {
+            AsyncImage(url: URL(string: product.imageURL)) { image in
+                image.resizable()
+            } placeholder: {
+                ProgressView()
+            }
+            .frame(height: 150)
+            
+            Text(product.title)
+            Text("$\(product.price, specifier: "%.2f")")
+        }
+        .padding()
+        .background(Color(.systemBackground))
+        .cornerRadius(10)
+    }
 }
